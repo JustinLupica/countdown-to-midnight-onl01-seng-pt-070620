37f958325a0f29ae4247aa8ec6a678780d397fbd
Justin Lupica <justinlupica@gmail.com>
2020-07-10 16:52:07 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -12,5 +12,7 @@ def countdown_with_sleep(number_of_seconds)
   while number_of_seconds > 0
   puts "#{number_of_seconds} SECOND(S)!"
   sleep(1)
+  number_of_seconds -= 1
 end
+"HAPPY NEW YEAR!"
 end
